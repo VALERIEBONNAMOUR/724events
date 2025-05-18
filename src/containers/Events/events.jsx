b5271671d993a3b3259365cce80c44424b4db800
Valerie BONNAMOUR <vale.bonnamour@gmail.com>
2025-05-18 11:07:41 +0200

    modifications fichiers

--- src/containers/Events/events.jsx
+++ src/containers/Events/events.jsx
@@ -7,57 +7,6 @@ import ModalEvent from "../ModalEvent/modalevent";
 
 import "./style.css";
 
-// import React, { useState, useEffect } from "react";
-// import { api } from "../../contexts/DataContext/datacontexts";
-
-// const Events = () => {
-//   const [events, setEvents] = useState([]);
-//   const [selectedCategory, setSelectedCategory] = useState(null);
-
-//   useEffect(() => {
-//     const fetchData = async () => {
-//       const data = await api.loadData();
-//       setEvents(data.events);
-//     };
-//     fetchData();
-//   }, []);
-
-//   const handleCategorySelect = (category) => {
-//     setSelectedCategory(category);
-//   };
-
-//   const filteredEvents = selectedCategory
-//     ? events.filter((event) => event.type === selectedCategory)
-//     : events;
-
-//   return (
-//     <div>
-//       <div>
-//         <button
-//           data-testid="collapse-button-testid"
-//           onClick={() => handleCategorySelect(null)}
-//         >
-//           Toutes les catégories
-//         </button>
-//         <button onClick={() => handleCategorySelect("soirée entreprise")}>
-//           Soirée entreprise
-//         </button>
-//         <button onClick={() => handleCategorySelect("forum")}>Forum</button>
-//       </div>
-//       <div>
-//         {filteredEvents.map((event) => (
-//           <div key={event.id}>
-//             <h3>{event.title}</h3>
-//             <p>{event.description}</p>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Events;
-
 const PER_PAGE = 9;
 
 const EventList = () => {
@@ -83,14 +32,7 @@ const EventList = () => {
     setType(evtType);
   };
 
-  // const totalEvents =
-  // data?.events.filter((event) => (type ? event.type === type : true))
-  //   .length || 0;
-
   const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
-  // const pageNumber = Math.ceil(totalEvents / PER_PAGE);
-  // const typeList = Array.from(new Set(data?.events.map((event) => event.type)));
-  // console.log("Data loaded:", data);
   const typeList = new Set(data?.events.map((event) => event.type));
   return (
     <>
@@ -100,11 +42,7 @@ const EventList = () => {
       ) : (
         <>
           <h3 className="SelectTitle">Catégories</h3>
-          {/* <Select
-            selection={typeList}
-            onChange={(value) => changeType(value)}
-            titleEmpty={false}
-            label="Type d'événement"></Select> */}
+      
           /<Select
             selection={Array.from(typeList)}
             onChange={(value) => (value ? changeType(value) : changeType(null))}
@@ -130,14 +68,6 @@ const EventList = () => {
               <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
                 {n + 1}
               </a>
-              //  {/* {Array.from({ length: pageNumber }, (_, n) => (
-              //   <a
-              //     key={`page-${n + 1}`}
-              //     href="#events"
-              //     onClick={() => setCurrentPage(n + 1)}
-              //   >
-              //     {n + 1}
-              //   </a> */}
             ))}
           </div>
         </>
